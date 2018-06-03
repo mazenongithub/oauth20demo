e0c8c3a75521410be0e94e1ee0e3ef9b7397a8c3
MazenonCloud9 <mazen@egeotechnical.com>
2018-06-03 23:58:29 +0000

    finished oauth google calendar demo

--- outhdemo.js
+++ outhdemo.js
@@ -6,6 +6,7 @@ const insert_uri = keys.googlecalenderinserturi;
 const fetchevents_uri = keys.googlecalenderfetchevents;
 const update_uri = keys.googlecalenderupdate;
 const findevent_uri = keys.googlecalenderfindevent;
+const eventdelete_uri = keys.googlecalendereventdelete;
 const client_id = keys.googleClientID;
 const client_secret = keys.googleClientSecret;
 const inserturi = 'https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=' + insert_uri +
@@ -20,13 +21,16 @@ const updateuri = 'https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=' +
 const findeventuri = 'https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=' + findevent_uri +
     '&prompt=consent&response_type=code&client_id=' + client_id + '&scope=https://www.googleapis.com/auth/calendar&access_type=offline'
 
+const eventdeleteuri = 'https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=' + eventdelete_uri +
+    '&prompt=consent&response_type=code&client_id=' + client_id + '&scope=https://www.googleapis.com/auth/calendar&access_type=offline'
 
 app.get('/', (req, res) => {
 
     res.send("<ul><li><a href=" + inserturi + "> Insert An Event  </a></li>" +
         "<li><a href=" + fetchevents + "> Show All my Events </a></li>" +
         "<li><a href=" + updateuri + "> Update Event </a></li>" +
-        "<li><a href=" + findeventuri + "> Find Event </a></ul>")
+        "<li><a href=" + findeventuri + "> Find Event </a>" +
+        "<li><a href=" + eventdeleteuri + "> Delete Event </a></ul>")
 })
 app.get('/oauth20/inserturi', (req, res) => {
     var grant_type = 'authorization_code';
@@ -239,6 +243,59 @@ app.get('/oauth20/findeventuri', (req, res) => {
                         res.send(body)
                     }
                     else {
+                        res.send(err)
+                    }
+                })
+
+
+            }
+            else {
+                res.send(err)
+            }
+
+
+        })
+})
+
+app.get('/oauth20/eventdeleteuri', (req, res) => {
+    var grant_type = 'authorization_code';
+    var code = req.query.code;
+
+    var values = "grant_type=" + grant_type +
+        "&code=" + code +
+        "&redirect_uri=" + eventdelete_uri +
+        "&client_id=" + client_id +
+        "&client_secret=" + client_secret;
+
+    request.post({
+            url: 'https://accounts.google.com/o/oauth2/token',
+            form: values,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        },
+        function(err, httpResponse, body) {
+            if (!err) {
+
+                body = JSON.parse(body);
+                var access_token = body.access_token;
+                var auth = "Bearer " + access_token;
+                var eventid = 'r2had1orehon5eth1tsqdqmg4g'
+                request.delete({
+                    url: 'https://www.googleapis.com/calendar/v3/calendars/immaisoncrosby@gmail.com/events/' + eventid,
+                    headers: {
+                        'Authorization': auth
+                    }
+
+                }, function(err, response, body) {
+                    if (!err) {
+
+                        //body = JSON.parse(body);
+                        console.log(response, body)
+                        res.send(body)
+                    }
+                    else {
+                        console.log(err)
                         res.send(err)
                     }
                 })
