8a8c8bca5a9b925b3c9b98274498900f8165b609
MazenonCloud9 <mazen@egeotechnical.com>
2018-06-05 06:09:54 +0000

    i just viewed my events using my service account

--- outhdemo.js
+++ outhdemo.js
@@ -1,3 +1,4 @@
+/*global Utilities*/
 const express = require("express");
 const app = express();
 const request = require("request");
@@ -314,44 +315,60 @@ app.get('/oauth20/eventdeleteuri', (req, res) => {
 })
 
 app.get('/oauth20/clientcredential/fetchevents', (req, res) => {
+    var private_key = keys.googleprivatekey;
     var iat = Math.round(new Date() / 1000);
     var exp = iat + 3600;
-    var sign = crypto.createSign('RSA-SHA256');
-    sign.update(keys.googleprivatekey); // data from your file would go here
-    var signature = sign.sign(keys.googleprivatekey, 'hex');
-    console.log(signature)
-    signature = base64url(signature);
+    // data from your file would go here
     var baseheader = { "alg": "RS256", "typ": "JWT" };
     baseheader = JSON.stringify(baseheader);
     baseheader = base64url(baseheader);
-    var myclaim = {
+    var claimSet = {
         "iss": "egeotechnical@egeotechnical-199216.iam.gserviceaccount.com",
         "scope": "https://www.googleapis.com/auth/calendar",
         "aud": "https://accounts.google.com/o/oauth2/token",
         "exp": exp,
-        "iat": iat,
-        "sub": "immaisoncrosby@gmail.com"
+        "iat": iat
     }
-    myclaim = JSON.stringify(myclaim);
-    myclaim = base64url(myclaim);
-    var jwt = baseheader + '.' + myclaim + '.' + signature;
+    claimSet = JSON.stringify(claimSet);
+    claimSet = base64url(claimSet)
+    const sign = crypto.createSign('RSA-SHA256');
+    //console.log(baseheader + "." + claimSet)
+    sign.update(baseheader + "." + claimSet);
 
-
+    var mysignature = sign.sign(keys.googleprivatekey, 'base64');
+    mysignature = encodeURIComponent(mysignature)
+    jwt = baseheader + "." + claimSet + "." + mysignature;
     var grant_type = 'urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer';
     var values = "grant_type=" + grant_type +
         "&assertion=" + jwt;
 
     request.post({
             url: 'https://accounts.google.com/o/oauth2/token',
-            form: values,
+            body: values,
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
             }
         },
         function(err, httpResponse, body) {
             if (!err) {
+                body = JSON.parse(body);
+                var access_token = body.access_token;
+                var auth = "Bearer " + access_token;
+                request({
+                    url: 'https://www.googleapis.com/calendar/v3/calendars/egeotechnical@egeotechnical-199216.iam.gserviceaccount.com/events',
+                    headers: {
+                        'Authorization': auth
+                    }
 
-                res.send(body)
+                }, function(err, response, body) {
+                    if (!err) {
+                        body = JSON.parse(body);
+                        res.send(body)
+                    }
+                    else {
+                        res.send(err)
+                    }
+                })
             }
             else {
 
