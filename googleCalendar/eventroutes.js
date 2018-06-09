b5c3a1095abce07199c4a725686cf43d2bf46f76
MazenonCloud9 <mazen@egeotechnical.com>
2018-06-09 20:08:52 +0000

    Basic Event API working

--- googleCalendar/eventroutes.js
+++ googleCalendar/eventroutes.js
@@ -340,6 +340,108 @@ module.exports = app => {
             })
     })
 
+    app.post('/googlecalendar/insertevent', (req, res) => {
+        var summary = req.body.summary;
+        var description = req.body.description;
+        var start = req.body.start;
+        var end = req.body.end;
+        var employee = req.body.employee;
+        var eventid = req.body.eventid;
+
+        var private_key = keys.googleprivatekey;
+        var iat = Math.round(new Date() / 1000);
+        var exp = iat + 3600;
+        // data from your file would go here
+        var baseheader = { "alg": "RS256", "typ": "JWT" };
+        baseheader = JSON.stringify(baseheader);
+        baseheader = base64url(baseheader);
+        var claimSet = {
+            "iss": "egeotechnical@egeotechnical-199216.iam.gserviceaccount.com",
+            "scope": "https://www.googleapis.com/auth/calendar",
+            "aud": "https://accounts.google.com/o/oauth2/token",
+            "exp": exp,
+            "iat": iat
+        }
+        claimSet = JSON.stringify(claimSet);
+        claimSet = base64url(claimSet)
+        const sign = crypto.createSign('RSA-SHA256');
+        //console.log(baseheader + "." + claimSet)
+        sign.update(baseheader + "." + claimSet);
+        var mysignature = sign.sign(keys.googleprivatekey, 'base64');
+        mysignature = encodeURIComponent(mysignature)
+        var jwt = baseheader + "." + claimSet + "." + mysignature;
+        var grant_type = 'urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer';
+        var values = "grant_type=" + grant_type +
+            "&assertion=" + jwt;
+
+        request.post({
+                url: 'https://accounts.google.com/o/oauth2/token',
+                body: values,
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            },
+            function(err, httpResponse, body) {
+                if (!err) {
+
+                    body = JSON.parse(body);
+                    var access_token = body.access_token;
+                    var auth = "Bearer " + access_token
+                    var event = {
+                        summary: summary,
+                        description: description,
+                        start: {
+                            dateTime: start
+                            //dateTime: "2018-06-07T19:00:00-07:00"
+                        },
+                        end: {
+                            dateTime: end
+                        },
+                        attendees: [{
+                            email: employee,
+                            responseStatus: "needsAction"
+                        }],
+                        visibility: "private",
+                        extendedProperties: {
+                            private: {
+                                everyoneDeclinedDismissed: "-1"
+                            }
+                        },
+                        guestsCanInviteOthers: false,
+                        reminders: {
+                            useDefault: true
+                        }
+                    }
+
+                    event = JSON.stringify(event)
+                    request.put({
+                        url: 'https://www.googleapis.com/calendar/v3/calendars/np4qfs6med0m5qpiv00peg48bk@group.calendar.google.com/events/' + eventid,
+                        body: event,
+                        headers: {
+                            'Authorization': auth,
+                            'Content-Type': 'application/json'
+                        }
+
+                    }, function(err, response, body) {
+                        if (!err) {
+                            body = JSON.parse(body);
+                            res.send(body)
+                        }
+                        else {
+                            res.send(err)
+                        }
+                    })
+
+
+                }
+                else {
+                    res.send(err)
+                }
+
+
+            })
+    })
+
     app.get('/googlecalendar/:eventid/deletevent', (req, res) => {
         var private_key = keys.googleprivatekey;
         var iat = Math.round(new Date() / 1000);
@@ -390,8 +492,7 @@ module.exports = app => {
                     }, function(err, response, body) {
                         if (!err) {
 
-                            //body = JSON.parse(body);
-                            console.log(response, body)
+                            res.send(response)
 
                         }
                         else {
