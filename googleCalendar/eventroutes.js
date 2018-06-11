b8ee429eb6e16905f60e5eea566c0905b6b89c98
MazenonCloud9 <mazen@egeotechnical.com>
2018-06-11 20:47:41 +0000

    heroku deploy

--- googleCalendar/eventroutes.js
+++ googleCalendar/eventroutes.js
@@ -1,13 +1,33 @@
 const request = require("request");
 const base64url = require("base64url");
-var crypto = require('crypto');
+var crypto = require('crypto-js');
 const keys = require("../keys");
 module.exports = app => {
 
     app.get('/googlecalendar/showallmyevents', (req, res) => {
         var private_key = keys.googleprivatekey;
         res.send(private_key)
-
+        var iat = Math.round(new Date() / 1000);
+        var exp = iat + 3600;
+        // data from your file would go here
+        var baseheader = { "alg": "RS256", "typ": "JWT" };
+        baseheader = JSON.stringify(baseheader);
+        baseheader = base64url(baseheader);
+        var claimSet = {
+            "iss": "egeotechnical@egeotechnical-199216.iam.gserviceaccount.com",
+            "scope": "https://www.googleapis.com/auth/calendar",
+            "aud": "https://accounts.google.com/o/oauth2/token",
+            "exp": exp,
+            "iat": iat
+        }
+        claimSet = JSON.stringify(claimSet);
+        claimSet = base64url(claimSet)
+        const sign = crypto.createSign('RSA-SHA256');
+        //console.log(baseheader + "." + claimSet)
+        sign.update(baseheader + "." + claimSet);
+        var mysignature = sign.sign(keys.googleprivatekey, 'base64');
+        mysignature = encodeURIComponent(mysignature)
+        res.send(mysignature)
     })
 
     app.get('/googlecalendar/:eventid/showmyevent', (req, res) => {
