4473415800966d7823caebe78b69dc8a15fd7713
MazenonCloud9 <mazen@egeotechnical.com>
2018-06-11 21:58:08 +0000

    updated access control

--- outhdemo.js
+++ outhdemo.js
@@ -29,7 +29,8 @@ const eventdeleteuri = 'https://accounts.google.com/o/oauth2/v2/auth?redirect_ur
     '&prompt=consent&response_type=code&client_id=' + client_id + '&scope=https://www.googleapis.com/auth/calendar&access_type=offline'
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://www.egeotechnical.com");
-
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
     next();
 });
 app.get('/', (req, res) => {
